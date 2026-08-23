6f4630818fce26bcce953d409fd95a497cf557b9
Mohd Badar <mohd.badar9917@gmail.com>
2026-08-23 22:11:13 +0530

    Update

---
+++ Demo-app/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  Demo-app
+//
+//  Created by Mohd Badar on 23/08/26.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
---
+++ Demo-app/SearchViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchViewController.swift
+//  Demo-app
+//
+//  Created by Mohd Badar on 23/08/26.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
